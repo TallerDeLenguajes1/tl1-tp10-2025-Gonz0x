544590a9703bd809bb1f721acc5d10a9de373146
Tomivega027 <tomivega027@gmail.com>
2025-06-17 13:12:32 -0300

    punto 1 con errores

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+using System.Text.Json;
+var tareas = await GetTareas();
+foreach (var tarea in tareas)
+{
+    Console.WriteLine($"TareaID: {tarea.id} - Descripcion: {tarea.titulo}")
+}
+
+static async Task<List<Tarea>> GetTareas()
+{    
+    var url = "https://jsonplaceholder.typicode.com/todos/";
+    try
+    {
+        HttpClient client = new HttpClient();
+        HttpResponseMessage response = await client.GetAsync(url);
+        response.EnsureSuccessStatusCode();
+        string responseBody = await response.Content.ReadAsStringAsync();
+        List<TAREA> coinDesk = JsonSerializer.Deserialize<List<Tarea>>(responseBody);
+        return coinDesk;   
+    }
+
+}
+catch (HttpRequestException e)
+{
+    Console.WriteLine("Problemas de acceso a la API");
+    Console.WriteLine("Message :{0} ", e.Message);
+    return null;
+}
+
---
+++ Tareas/tarea.cs
@@ -0,0 +1,14 @@
+    public class Tarea
+    {
+        [JsonPropertyName("userId")]
+        public int IdUsuario { get; set; }
+
+        [JsonPropertyName("id")]
+        public int id { get; set; }
+
+        [JsonPropertyName("title")]
+        public string titulo { get; set; }
+
+        [JsonPropertyName("completed")]
+        public bool completada { get; set; }
+    }
